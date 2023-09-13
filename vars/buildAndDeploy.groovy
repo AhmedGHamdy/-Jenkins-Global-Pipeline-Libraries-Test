7f96dae6789219e9160606d482d512b97a3e37a1
Ahmed Gamal <60747498+AhmedGHamdy@users.noreply.github.com>
2023-09-13 22:12:43 +0300

    Update buildAndDeploy.groovy

--- vars/buildAndDeploy.groovy
+++ vars/buildAndDeploy.groovy
@@ -1,50 +1,14 @@
-// vars/buildAndDeploy.groovy
+// my-shared-library/vars/buildAndDeploy.groovy
 
-def call(Map pipelineConfig) {
-    def deployName = pipelineConfig.deployName
-    def repoName = pipelineConfig.repoName
-    def stage = pipelineConfig.stage
-    def version = pipelineConfig.version
+def call(Map parameters) {
+    def deployName = parameters.deployName
+    def repoName = parameters.repoName
 
-    pipeline {
-        agent any
-        environment {
-            env.deployName = deployName
-            env.repoName = repoName
-        }
-        stages {
-            stage('Build') {
-                steps {
-                    script {
-                      sh 'echo "Building"'
-                    }
-                }
-            }
-            stage('Create Image') {
-                steps {
-                    sh 'echo "Create Image"'
-                }
-            }
-            stage('Deploy To Build') {
-                steps {
-                    script {
-                        if (stage == 'dev') {
-                            sh 'echo "Deploying to dev environment"'
-                        } else if (stage == 'test') {
-                            sh 'echo "Deploying to test environment"'
-                        } else if (stage == 'release') {
-                            sh 'echo "Deploying to release environment"'
-                        } else {
-                            error "Invalid value for stage parameter: '${stage}'"
-                        }
-                    }
-                }
-            }
-        }
-        post {
-            success {
-                sh "printenv"
-            }
-        }
-    }
+    // Define environment variables for use in the calling pipeline
+    env.DEPLOY_NAME = deployName
+    env.REPO_NAME = repoName
+
+    // Print some information for testing purposes
+    echo "Deploy Name: ${DEPLOY_NAME}"
+    echo "Repo Name: ${REPO_NAME}"
 }
