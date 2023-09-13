010857143706e2764ee76a9ba29a09e591c8bf39
Ahmed Gamal <60747498+AhmedGHamdy@users.noreply.github.com>
2023-09-13 21:24:24 +0300

    Update buildAndDeploy.groovy

--- vars/buildAndDeploy.groovy
+++ vars/buildAndDeploy.groovy
@@ -1,6 +1,6 @@
 def call(Map parameters) {
-    def deployName = parameters.deployName
-    def repo = parameters.repo
+    def deployName = parameters.deployName ?: 'DefaultDeployName'
+    def repo = parameters.repo ?: 'DefaultRepo'
 
     pipeline {
         agent any
@@ -11,20 +11,26 @@ def call(Map parameters) {
         stages {
             stage('Build') {
                 steps {
-                    echo "Building ${DEPLOY_NAME} using repo value: ${REPO}"
-                    // Add your build steps here
+                    script {
+                        echo "Building ${DEPLOY_NAME} using repo value: ${REPO}"
+                        // Add your build steps here
+                    }
                 }
             }
             stage('Create Image') {
                 steps {
-                    echo "Creating image for ${DEPLOY_NAME} using repo value: ${REPO}"
-                    // Add your image creation steps here
+                    script {
+                        echo "Creating image for ${DEPLOY_NAME} using repo value: ${REPO}"
+                        // Add your image creation steps here
+                    }
                 }
             }
             stage('Deploy To Build') {
                 steps {
-                    echo "Deploying to ${DEPLOY_NAME} using repo value: ${REPO}"
-                    // Add your deployment steps here
+                    script {
+                        echo "Deploying to ${DEPLOY_NAME} using repo value: ${REPO}"
+                        // Add your deployment steps here
+                    }
                 }
             }
         }
