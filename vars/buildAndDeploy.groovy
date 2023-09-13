a3f0f5f2c9698c79f7d12650d4220285a5ab3523
Ahmed Gamal <60747498+AhmedGHamdy@users.noreply.github.com>
2023-09-13 22:15:39 +0300

    Update buildAndDeploy.groovy

--- vars/buildAndDeploy.groovy
+++ vars/buildAndDeploy.groovy
@@ -1,14 +1,42 @@
 // my-shared-library/vars/buildAndDeploy.groovy
 
 def call(Map parameters) {
-    def deployName = parameters.deployName
-    def repoName = parameters.repoName
+    def deployName = parameters.deployName ?: 'DefaultDeployName'
+    def repoName = parameters.repoName ?: 'DefaultRepoName'
 
-    // Define environment variables for use in the calling pipeline
-    env.DEPLOY_NAME = deployName
-    env.REPO_NAME = repoName
+    // Define environment variables using 'withEnv'
+    withEnv([
+        "DEPLOY_NAME=${deployName}",
+        "REPO_NAME=${repoName}"
+    ]) {
+        // Print some information for testing purposes
+        echo "Deploy Name: ${DEPLOY_NAME}"
+        echo "Repo Name: ${REPO_NAME}"
 
-    // Print some information for testing purposes
-    echo "Deploy Name: ${DEPLOY_NAME}"
-    echo "Repo Name: ${REPO_NAME}"
+        // Perform your actual pipeline steps here
+        stage('Build') {
+            steps {
+                script {
+                    echo "Building ${DEPLOY_NAME} using repo value: ${REPO_NAME}"
+                    // Add your build steps here
+                }
+            }
+        }
+        stage('Create Image') {
+            steps {
+                script {
+                    echo "Creating image for ${DEPLOY_NAME} using repo value: ${REPO_NAME}"
+                    // Add your image creation steps here
+                }
+            }
+        }
+        stage('Deploy To Build') {
+            steps {
+                script {
+                    echo "Deploying to ${DEPLOY_NAME} using repo value: ${REPO_NAME}"
+                    // Add your deployment steps here
+                }
+            }
+        }
+    }
 }
